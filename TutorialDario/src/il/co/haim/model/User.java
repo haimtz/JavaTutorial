00406092204171faef92b23a1b0343cc71d33b80
Haim <haimtzabar@gmail.com>
2013-05-08 11:33:06 +0300

    Signed-off-by: Haim <haimtzabar@gmail.com>

--- TutorialDario/src/il/co/haim/model/User.java
+++ TutorialDario/src/il/co/haim/model/User.java
@@ -7,5 +7,39 @@ public class User {
 	private String Email;
 	private String Tel;
 	private String Password;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getIdNumber() {
+		return IdNumber;
+	}
+	public void setIdNumber(String idNumber) {
+		IdNumber = idNumber;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getTel() {
+		return Tel;
+	}
+	public void setTel(String tel) {
+		Tel = tel;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+	
 
 }
