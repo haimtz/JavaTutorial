19c61931b5e342894604525d340bbafa23740a63
HOME <haimtzabar@gmail.com>
2013-05-10 23:37:30 +0300

    Friday to delete the test Signed-off-by: HOME <haimtzabar@gmail.com>

--- TutorialDario/src/Tester/TesterClass.java
+++ TutorialDario/src/Tester/TesterClass.java
@@ -2,7 +2,14 @@ package Tester;
 
 
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Formatter;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,21 +20,31 @@ import model.*;
 
 public class TesterClass {
 	
+	static List<User> fromFile = new LinkedList<User>();
+	
 	public static void main(String[] args) throws Exception {
 		
-		List<User> myUsers = new LinkedList<>();
-		
-		myUsers.add(new User("ABS","123","pass"));
-		myUsers.add(new User("ABS1","1234","pass1"));
-		myUsers.add(new User("ABS2","12345","pass2"));
-		myUsers.add(new User("ABS3","123456","pass3"));
+		List<User> myUsers = new LinkedList<User>();
 		
 		
-		Formatter output;
+		
+		myUsers.add(new User("SDfdhfjk","123","pass"));
+		myUsers.add(new User("abcd","123","pass"));
+		
+		
+		ObjectOutputStream  output;
+		File storageFile = new File("C:\\Users\\HOME\\Desktop\\User.txt");
+		
 		
 		try
 		{
-			output = new Formatter("User.txt");
+			output = new ObjectOutputStream (new FileOutputStream("C:\\Users\\HOME\\Desktop\\User.txt"));
+			
+			for(User u : myUsers)
+			{
+				output.writeObject(u);
+			}
+			output.close();
 		}
 		 // end try
 		catch ( SecurityException securityException )
@@ -41,7 +58,36 @@ public class TesterClass {
 		       System.err.println( "Error creating file." );
 		          System.exit( 1 );
 		} 
+		
 
+		read();
+		
+		for(User u : fromFile)
+		{
+			System.out.println(u.getName() + " " + u.getIdNumber());
+		}
+
+	}
+	
+	public static void read() throws FileNotFoundException, IOException, ClassNotFoundException
+	{
+		ObjectInputStream input;
+		
+		try{
+			input = new ObjectInputStream(new FileInputStream("C:\\Users\\HOME\\Desktop\\User.txt"));
+			
+			while(true)
+			{
+				User temp;
+				temp = (User)input.readObject();
+				fromFile.add(temp);
+			}
+		}
+
+		catch (EOFException endOfFileException )
+		{
+			return;
+		}
 	}
 
 }
